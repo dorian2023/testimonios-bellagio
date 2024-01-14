20dc45db156f9eb2a7f9e694b7978769f0a89548
dorian2023 <doriangonzalez2018@gmail.com>
2024-01-14 02:00:51 -0400

    React-Testimonios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+//COMPONENTE PRINCIPAL DE LA APLICACION
 import './App.css';
+import Testimonio from "./componentes/Testimonio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Muebles Bellago - Testimonios</h1>
+        <Testimonio
+          nombre="Dorian Gonzalez"
+          pais="Venezuela"
+          imagen="dorian"
+          cargo="Desarrollador"
+          empresa="Mueble Bellagio"
+          testimonio="lorem ssssssssssssssssssss ssssssssssssssssssssssss sssssssssssssssssssssssssssssssss ssssssssssssssssssssssss s"
+        />
+        <Testimonio
+          nombre="Jeannette Santander"
+          pais="Venezuela"
+          imagen="dorian2"
+          cargo="Ama de Casa"
+          empresa="Mi casa"
+          testimonio="losssssssssssssssssssssssssssrem ssssssssssssssssssss ssssssssssssssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssss ssssssssssssssssssssssss s"
+        />
+        <Testimonio
+          nombre="Reynaldo Gonzalez"
+          pais="Colombia"
+          imagen="dorian3"
+          cargo="Ingeniero"
+          empresa="Google"
+          testimonio="losssssssssssssssssssssssssssrem ssssssssssssssssssss ssssssssssssssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssss ssssssssssssssssssssssss s"
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+{/* {} <></> */}
